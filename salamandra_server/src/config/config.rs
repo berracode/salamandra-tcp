f9e6445bf3190329360306b0eabaea8529f6aa91
berracode <juanguillermoalarcon@gmail.com>
2023-02-23 23:01:37 -0500

    Se agrega clase config, se agrega actix

---
+++ salamandra_server/src/config/config.rs
@@ -0,0 +1,76 @@
+
+use std::{fs::File, io::Read};
+
+use local_ip_address::local_ip;
+use serde::{Serialize, Deserialize};
+
+
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Config {
+    /// configuración del cliente
+    #[serde(rename = "server")]
+    pub server: Server,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Server {
+    /// Socket addresses where this server listens.
+    pub listen: Sock,
+
+    /// server name
+    pub name: Option<String>,
+
+    pub base_route: Option<String>,
+
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Sock{
+    pub ip: Option<String>,
+    pub port: Option<String>
+}
+
+impl Config {
+
+    pub fn new() -> Config  {
+        let mut file_content = String::new();
+        let mut file = File::open("server.toml").unwrap();
+        file.read_to_string(&mut file_content).unwrap();
+        let mut config: Config = toml::from_str(&file_content).unwrap();
+        let my_local_ip = local_ip().unwrap().to_string();
+        config.server.listen.ip = Some(my_local_ip);
+        
+        println!("config {:?}", config.get_listener());
+        config
+    }
+
+    pub fn get_listener(&self) -> String {
+        let client = &self.server;
+        let listen = &client.listen;
+        let local_ip_address = listen.ip.clone();
+        let local_port = listen.port.clone();
+
+        let mut local_ip_address = local_ip_address.unwrap();
+        let local_port = local_port.unwrap();
+        local_ip_address.push_str(":");
+        local_ip_address.push_str(&local_port);
+
+        local_ip_address.clone()
+    }
+
+    pub fn get_ip(&self) -> String {
+        let server = &self.server;
+        let ip = server.listen.ip.clone();
+        ip.unwrap()
+    }
+
+    pub fn get_port(&self) -> u16 {
+        let server = &self.server;
+        let port = server.listen.port.clone();
+        let port = port.unwrap();
+        port.parse::<u16>().unwrap()
+        
+    }
+    
+}
--- salamandra_server/src/main.rs
+++ salamandra_server/src/main.rs
@@ -1,163 +1,67 @@
-use std::{net::{TcpListener, TcpStream}, thread, io::{BufReader, BufRead, Write, Read}, fs::{self, File}};
+use actix_files::NamedFile;
+use salamandra_server::config::config::Config;
+use std::{net::{TcpListener, TcpStream}, thread, io::{BufReader, Write},  time::Duration};
 use std::io::{ Result};
-use std::str;
-
-use encoding::{Encoding, EncoderTrap};
-use encoding::all::ASCII;
-use chrono::Utc;
-
-const BUFFER_SIZE: usize = 51_200;
-
-fn main() {
-    println!("Hello, world server!");
-    start_server();
+use actix_web::{HttpServer, App, Responder, HttpResponse, get, web, HttpRequest};
 
 
+
+
+async fn index(req: HttpRequest) -> Result<NamedFile> {    
+    let mut path = ".".to_string();
+    path.push_str(req.path());
+
+    let file = NamedFile::open(path).unwrap();
+
+    Ok(file)
 }
 
-fn process_connection(mut stream: TcpStream){
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
 
 
-    let mut buf_reader = BufReader::new(&stream);
-    //thread::sleep(Duration::from_secs(10));
-    let mut final_line = String::new();
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
-
-    println!("Request: {:#?}", http_request);
-
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("index.html").unwrap();
-    let length = contents.len();
-
-    let response =
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
+fn process_connection(mut stream: TcpStream, config: Config){
+    println!("un nuevo cliente conectado desde {:?}", stream);
+    thread::sleep(Duration::from_secs(10));
+    let response = "HOLA CLIENTE";
     stream.write_all(response.as_bytes()).unwrap();
-    println!("termina cliente {:?}", stream)
+    println!("chao {:?}", stream)
+   
 
 }
 
-fn encode_message_size(cmd: &str) -> Result<Vec<u8>> {
-    let mut message_size = cmd.len();
-    //println!("{:?}", cmd);
-    message_size = message_size + 1;
-    let message_size_str = message_size.to_string();
-    let mut message_size_bytes = ASCII.encode(&message_size_str, EncoderTrap::Strict).map_err(|x| x.into_owned()).unwrap();
-    message_size_bytes.push('\r' as u8);
 
-    //Ok(String::from_utf8(string_size_bytes).unwrap())
-    Ok(message_size_bytes)
-}
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let config= Config::new();
+    let ip = config.get_ip();
+    let port = config.get_port();
 
-fn encode_message(cmd: &str) -> Result <Vec<u8>> {
-    //println!("{:?}", cmd);
-    let message_str = cmd.to_string();
-    let mut message_bytes = ASCII.encode(&message_str, EncoderTrap::Strict).map_err(|x| x.into_owned()).unwrap();
-    message_bytes.push('\r' as u8);
-
-    //Ok(String::from_utf8(string_size_bytes).unwrap())
-    Ok(message_bytes)
-}
-
-fn check_ack(mut ack_buf: &mut [u8]) -> String {
-
-    let ack_slice: &str = str::from_utf8(&mut ack_buf).unwrap(); //string slice
-    let mut ack_str = ack_slice.to_string(); //convert slice to string
-    let index: usize = ack_str.rfind('\r').unwrap();
-    //println!("{:?} server ACK:", ack_str.split_off(index));
-    format!("{:?}", ack_str.split_off(index)); 
-    if ack_str != "ACK"{
-        //println!("received ACK from server");
-        // end with error, maybe set a timeout
-        return String::from("error")
-    }
-    String::from("ACK")
-}
-
-fn send_file_to_client(){
-    println!("enviar binario a cliente");
-    // conectarse al cliente
-    // leer archivo
-    // enviar archivo, escribir en el cliente
-
+    thread::spawn(move|| {
+        let listener = TcpListener::bind(config.get_listener()).unwrap();
     
-    let mut stream = TcpStream::connect("192.168.1.161:9090") // try!(TcpStream::connect(HOST));
-    .expect("Couldn't connect to the server...");
+        println!("listening started, ready to accept");
+        for stream in listener.incoming() {
+            let config = config.clone();
 
-    let full_path = String::from("./shared/principal.exe");
-
-
-    let mut file = File::open(full_path).unwrap();
-    let file_size = file.metadata().unwrap().len();
-    println!("file_size {:?}", file_size);
-    let mut ack_buf = [0u8; 8];
-
-
-    //send file size
-    let encoded_file_size = encode_message(&file_size.to_string()).unwrap();
-    stream.write_all(&encoded_file_size).unwrap();
-
-     //receive ack
-     stream.read(&mut ack_buf).unwrap();
-     if check_ack(&mut ack_buf) != "ACK" { println!("get_file ACK Failed"); }
-     println!("[recibido] ACK");
-
-    let mut buf = [0u8; BUFFER_SIZE];
-
-    let mut remaining_data = file_size as i32;
-    let ini = Utc::now();
-    while remaining_data != 0 {
-
-        if remaining_data >= BUFFER_SIZE.try_into().unwrap() {
-
-            let file_slab = file.read(&mut buf);
-            match file_slab{
-                Ok(n) => {
-                    stream.write_all(&buf).unwrap();
-                    remaining_data = remaining_data - n as i32;
-                    println!("sent {} file bytes (big) | remaining: {}", n, remaining_data);
-
-                }
-                _ => {}
-            }
-        } else {
-            let file_slab = file.read(&mut buf);
-            match file_slab {
-                //client must shrink this last buffer
-                Ok(n) => {
-
-                    let mut r_slice = &buf[0..(remaining_data as usize)]; //fixes underreading
-
-                    stream.write_all(&r_slice).unwrap();
-                    remaining_data = remaining_data - n as i32;
-                    println!("sent {} file bytes (small) | remaining: {}", n, remaining_data);
-                }
-                _ => {}
-            }
+            thread::spawn(|| {
+                let stream = stream.unwrap();
+                process_connection(stream, config);
+            });
         }
-        
-    }
-    let fin = Utc::now();
-    println!("Tiempo transcurrido: {}", (fin.signed_duration_since(ini)));
+    
+    });
 
-
-
+    HttpServer::new(|| {
+        App::new()
+            .route("/shared/{filename:.*}", web::get().to(index))
+    })
+    .bind((ip, port))?
+    .run()
+    .await
 }
 
-fn start_server(){
-    let listener = TcpListener::bind("127.0.0.1:8082").unwrap();
-    println!("listening started, ready to accept");
 
-    send_file_to_client();
 
-    /*for stream in listener.incoming() {
-        thread::spawn(|| {
-            let mut stream = stream.unwrap();
-            process_connection(stream);
-        });
-    }*/
-}
